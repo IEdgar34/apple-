ef97a866e5581224dc208ec39451140c9fb6b7c8
edgar <edo.jan.94@bk.ru>
2026-04-08 21:15:00 +0300

    apple

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import "~/style.css";
+import "~/font.css";
+import "./assets/scss/style.scss";
+window.addEventListener("DOMContentLoaded", () => {
+	const burgerBtn = document.querySelector(".burger");
+	const navList = document.querySelector(".nav-list-mob");
+	const wrapper = document.querySelector(".wrapper");
+	const ftContent = document.querySelectorAll(".ft-content");
+	const ftContentList = document.querySelector(".ft-content__list");
+	const ftContentBtn = document.querySelector(".ft-content-btn");
+
+	burgerBtn.addEventListener("click", () => {
+		navList.classList.toggle("nav-list-mob_active");
+		burgerBtn.classList.toggle("burger_active");
+		document.body.classList.toggle("overflow");
+
+		let attr =
+			navList.getAttribute("aria-hidden") === "true" ? "false" : "true";
+		navList.setAttribute("aria-hidden", `${attr}`);
+	});
+
+	ftContent.forEach((item) => {
+		item.addEventListener("click", (e) => {
+			let child = e.currentTarget.querySelector(".ft-content__list");
+			let ftContentBtn = e.currentTarget.querySelector(".ft-content-btn ");
+            child.classList.toggle('open')
+            ftContentBtn.classList.toggle('ft-content-btn_rotate')
+			if (child.classList.contains('open')) {
+				child.style = `
+                height: ${child.scrollHeight}px;
+                scale:1;
+                pointer-events: unset;
+                transition: height 0.3s ,scale 0.5s;
+                position:static;
+            `;
+			}else {
+                 child.style = `
+               
+            `
+            }
+		});
+	});
+});
